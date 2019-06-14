f6745bfc0c97ba744b247ae02ea64acdb0ef6a0d
Daniil <anatoliikljukin@mail.ru>
2019-06-14 23:58:48 +0400

    Add TaskWorker.cs
    Solved second task (recursive search for the maximum element in the array), added NUnit tests for  FindMaxElementInArray method.

---
+++ TasksLibrary/TaskWorker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TasksLibrary
+{
+    public class TaskWorker
+    {
+        /// <summary>
+        /// Method for searching the maximum element of array
+        /// </summary>
+        /// <param name="array">>Array in which we search maximum</param>
+        /// <returns>Maximum element of an integer array</returns>
+        public int FindMaxElementInArray(int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+
+            return DoRecursiveSearch(array, int.MinValue, 0);
+        }
+
+        /// <summary>
+        /// Method for recursively searching the maximum element of array
+        /// </summary>
+        /// <param name="array">Array in which we search maximum</param>
+        /// <param name="maxElement">Current maximum element of the array</param>
+        /// <param name="index">Index of current max element</param>
+        /// <returns></returns>
+        private int DoRecursiveSearch(int[] array, int maxElement, int index)
+        {
+            if (index < array.Length)
+            {
+                if (array[index] > maxElement)
+                    maxElement = array[index];
+
+                return DoRecursiveSearch(array, maxElement, ++index);
+            }
+            else
+                return maxElement;
+        }
+    }
+}
